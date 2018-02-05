e62c16893b32cb1e5c3e193373b3a173a3ece0b0
amstoler <amstoler@gmail.com>
2018-02-05 15:28:12 -0500

    Added Mapping for Request, Get, and Post. Created empty CosForm html page.

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,4 +1,43 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
+
+@Controller
 public class HomeController {
+    @Autowired
+    CosRepository cosRepository;
+
+    @RequestMapping("/")
+    public String listCosmetics(Model model){
+        model.addAttribute("cosmetics", cosRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String cosForm(Model model){
+        model.addAttribute("cosmetic", new Cosmetic());
+        return "cosForm";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Cosmetic cosmetic, BindingResult result){
+
+        if (result.hasErrors()){
+            return "cosForm";
+        }
+        cosRepository.save(cosmetic);
+        return "redirect:/";
+
+    }
+
+
 }
